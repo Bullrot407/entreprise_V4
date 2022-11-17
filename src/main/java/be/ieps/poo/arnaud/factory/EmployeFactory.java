5704d0fa8bbc3ff00d640f604981c5aa6661f809
Bullrot407 <Bullrot407@gmail.com>
2022-11-17 19:33:06 +0100

    Ajout des tests unitaire

--- src/main/java/be/ieps/poo/arnaud/factory/EmployeFactory.java
+++ src/main/java/be/ieps/poo/arnaud/factory/EmployeFactory.java
@@ -7,13 +7,20 @@ public class EmployeFactory {
 
     public Personne getPersonne(String nom, String prenom, Profession profession, int id) throws EntrepriseException {
         return switch (profession) {
-            case DIRECTEUR -> new Directeur(nom, prenom, 0f, id);
+            /*case DIRECTEUR -> new Directeur(nom, prenom, 0f, id);*/
             case SECRETAIRE -> new Secretaire(nom, prenom, id);
             case COMPTABLE -> new Comptable(nom, prenom, id);
             case MACON -> new Macon(nom, prenom, id);
             case MACONQUALIFIE -> new MaconQualifie(nom, prenom, id);
             case MANOEUVRE -> new Manoeuvre(nom, prenom, id);
-            case CONTREMAITRE -> new Contremaitre(nom,prenom,id);
+            case CONTREMAITRE -> new Contremaitre(nom, prenom, id);
+            default -> throw new EntrepriseException("La profession ne peut pas etre NULL");
+        };
+    }
+
+    public Personne getPersonne(String nom, String prenom, Profession profession, float porteFeuille, int id) throws EntrepriseException {
+        return switch (profession) {
+            case DIRECTEUR -> new Directeur(nom, prenom, 0f, id);
             default -> throw new EntrepriseException("La profession ne peut pas etre NULL");
         };
     }
--- src/test/java/be/ieps/poo/arnaud/controller/EnterpriseControllerTest.java
+++ src/test/java/be/ieps/poo/arnaud/controller/EnterpriseControllerTest.java
@@ -2,10 +2,12 @@ package be.ieps.poo.arnaud.controller;
 
 import be.ieps.poo.arnaud.exceptions.EntrepriseException;
 import be.ieps.poo.arnaud.factory.EmployeFactory;
-import be.ieps.poo.arnaud.model.Entreprise;
-import be.ieps.poo.arnaud.model.Personne;
-import be.ieps.poo.arnaud.model.Profession;
+import be.ieps.poo.arnaud.model.*;
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,18 +27,75 @@ class EnterpriseControllerTest {
     @Test
     @Order(1)
     void ajoutEmployeEntreprise() throws EntrepriseException {
-        //ajout directeur
-        pers = factory.getPersonne("Lebic", "Bobile", Profession.DIRECTEUR, 001);
+        //ajout directeur (1)
+        pers = factory.getPersonne("Boss", "Jean", Profession.DIRECTEUR, 1500, 001);
         entreprise.ajouterEmploye(pers);
-        //ajout secretaire
-        pers = factory.getPersonne("Lebic", "Bobile", Profession.SECRETAIRE, 100);
+        //ajout secretaire (2)
+        pers = factory.getPersonne("Bureau", "Claudia", Profession.SECRETAIRE, 100);
         entreprise.ajouterEmploye(pers);
-        pers = factory.getPersonne("Lebrun", "Jule", Profession.SECRETAIRE, 101);
+        pers = factory.getPersonne("Mercury", "Julie", Profession.SECRETAIRE, 101);
+        entreprise.ajouterEmploye(pers);
+        //ajout comptable (1)
+        pers = factory.getPersonne("Chiffre", "Max", Profession.COMPTABLE, 200);
+        entreprise.ajouterEmploye(pers);
+        //ajout contremaitre (1)
+        pers = factory.getPersonne("Dujardin", "Julien", Profession.CONTREMAITRE, 300);
+        entreprise.ajouterEmploye(pers);
+        //ajout macon (3)
+        pers = factory.getPersonne("Ciment", "Linda", Profession.MACON, 400);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("truelle", "Romuald", Profession.MACON, 401);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("Placard", "Christopher", Profession.MACON, 402);
+        entreprise.ajouterEmploye(pers);
+        //ajout maconqualifie (2)
+        pers = factory.getPersonne("Piperzel", "Lionel", Profession.MACONQUALIFIE, 500);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("Chaudron", "Michel", Profession.MACONQUALIFIE, 501);
+        entreprise.ajouterEmploye(pers);
+        //ajout manoeuvre (4)
+        pers = factory.getPersonne("Ducamion", "Gauthier", Profession.MANOEUVRE, 600);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("Carref", "Maxime", Profession.MANOEUVRE, 601);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("Dutemps", "Marc", Profession.MANOEUVRE, 602);
+        entreprise.ajouterEmploye(pers);
+        pers = factory.getPersonne("LeCourte", "Arno", Profession.MANOEUVRE, 603);
         entreprise.ajouterEmploye(pers);
     }
-
     @Test
     @Order(2)
+    void verifieDonneeDirecteur() {
+        List<Directeur> expectedDirecteurList = Arrays.asList(
+                new Directeur("Boss", "Jean",1500, 001)
+        );
+
+        ArrayList<Personne> actualDirecteurList = entreprise.getListeEmploye().get(Profession.DIRECTEUR);
+        assertEquals(expectedDirecteurList, actualDirecteurList);
+    }
+    @Test
+    @Order(3)
+    void verifieDonneeSecretaire() {
+        List<Secretaire> expectedSecretaireList = Arrays.asList(
+                new Secretaire("Bureau", "Claudia", 100),
+                new Secretaire("Mercury", "Julie", 101)
+        );
+
+        ArrayList<Personne> actualSecretaireList = entreprise.getListeEmploye().get(Profession.SECRETAIRE);
+        assertEquals(expectedSecretaireList, actualSecretaireList);
+    }
+    @Test
+    @Order(4)
+    void verifieDonneeComptable() {
+        List<Comptable> expectedComptableList = Arrays.asList(
+                new Comptable("Chiffre", "Max", 200)
+        );
+
+        ArrayList<Personne> actualComptableList = entreprise.getListeEmploye().get(Profession.COMPTABLE);
+        assertEquals(expectedComptableList, actualComptableList);
+    }
+    @Test
+    @Order(5)
     void testTropDEmployee() {
         try {
             pers = factory.getPersonne("Ruelle", "Max", Profession.SECRETAIRE, 102);
@@ -49,13 +108,13 @@ class EnterpriseControllerTest {
     }
 
     @Test
-    @Order(3)
+    @Order(6)
     void testSuppDEmployee() {
         System.out.println("affichage avant suppression");
         entreprise.afficherEntreprise();
-        assertEquals(2, entreprise.nombreEmpEntreprise());
+        assertEquals(14, entreprise.nombreEmpEntreprise());
         entreprise.supprimerPersonne(101);
-        assertEquals(1, entreprise.nombreEmpEntreprise());
+        assertEquals(13, entreprise.nombreEmpEntreprise());
         System.out.println("\naffichage apres suppression");
         entreprise.afficherEntreprise();
 
